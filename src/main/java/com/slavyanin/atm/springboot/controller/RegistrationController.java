ad1a2e68f70d87ad3a20ab4daaa942979fbd01f2
Alexander Usov <a.usoff@gmail.com>
2016-07-10 12:04:34 +0300

    Spring Security. Application can open by login and password but don`t have access to sql.

---
+++ src/main/java/com/slavyanin/atm/springboot/controller/RegistrationController.java
@@ -0,0 +1,75 @@
+package com.slavyanin.atm.springboot.controller;
+
+import com.slavyanin.atm.springboot.service.UserService;
+import com.slavyanin.atm.springboot.utils.Ajax;
+import com.slavyanin.atm.springboot.utils.ExceptionHandlerController;
+import com.slavyanin.atm.springboot.utils.RestException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class RegistrationController extends ExceptionHandlerController {
+
+    private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
+
+    @Autowired
+    @Qualifier("userService")
+    private UserService userService;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    Map<String, Object> insert(@RequestParam("data") String userParam) throws RestException {
+
+        log.info(userParam);
+
+        JSONParser parser = new JSONParser();
+        try {
+            if (userParam == null || userParam.equals("")) {
+                return Ajax.emptyResponse();
+            }
+
+            Object obj = parser.parse(userParam);
+            JSONObject jsonObject = (JSONObject) obj;
+            List<String> arr = new ArrayList<>();
+            arr.add((String) jsonObject.get("name"));
+            arr.add((String) jsonObject.get("email"));
+            arr.add((String) jsonObject.get("password"));
+
+            userService.insert(arr);
+
+            log.info((String) jsonObject.get("email"));
+
+            return Ajax.emptyResponse();
+        } catch (Exception e) {
+            log.error(String.valueOf(e));
+            throw new RestException(e);
+        }
+    }
+
+//    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+//    public
+//    @ResponseBody
+//    Map<String, Object> getRandomData() throws RestException {
+//        try {
+//            Set<User> result = userService.findAll();
+//            return Ajax.successResponse(result);
+//        } catch (Exception e) {
+//            throw new RestException(e);
+//        }
+//    }
+
+}
--- src/main/java/com/slavyanin/atm/springboot/controller/UserController.java
+++ src/main/java/com/slavyanin/atm/springboot/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,8 +59,6 @@ public class UserController extends ExceptionHandlerController {
             arr.add((String) jsonObject.get("password"));
 
             userService.insert(arr);
-
-            log.info((String) jsonObject.get("email"));
 
             return Ajax.emptyResponse();
         } catch (Exception e) {
@@ -105,6 +104,38 @@ public class UserController extends ExceptionHandlerController {
         } catch (Exception e) {
             throw new RestException(e);
         }
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Custom Login Form");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+
+    }
+
+    //Spring Security see this :
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+        return model;
+
     }
 
 }
